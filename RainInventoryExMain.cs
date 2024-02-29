75286c536bbed05e7c4c8dfc8d6abf09f26f6637
Winston Kirk <152569394+winston-kirk@users.noreply.github.com>
2024-02-29 02:03:58 +0100

    feat: basic project & plugin

---
+++ RainInventoryExMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+
+namespace RainInventoryEx
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class RainInventoryExMain : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "winston_kirk.raininventoryex";
+        public const string PLUGIN_NAME = "RainInventoryEx - Inventory System";
+        public const string PLUGIN_VERSION = "0.0.1";
+
+        private void OnEnable()
+        {
+            Console.WriteLine("RainInventoryEx - Hello RainCraftEx! Yes my dear friend, I'm here to help you out!");
+        }
+    }
+}
